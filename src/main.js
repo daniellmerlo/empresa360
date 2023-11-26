c3094d6bd7826c568b3e183448f070d8f4d5e277
Daniel Merlo <daniellmerlo@gmail.com>
2023-11-25 21:47:34 -0300

    216. Configurando o Vue Router em um projeto Vue 3

--- src/main.js
+++ src/main.js
@@ -3,4 +3,26 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from '@/views/Home.vue'
+import Login from '@/views/Login.vue'
+
+const routes = [
+    {
+        path: '/home',
+        component: Home
+    },
+    {
+        path: '/login',
+        component: Login
+    }
+]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes
+})
+
+const Vue = createApp(App)
+Vue.use(router)
+Vue.mount('#app')
