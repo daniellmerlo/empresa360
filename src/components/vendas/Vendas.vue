56fff55fcd5036d52824538f6904c7d8252895d3
Daniel Merlo <daniellmerlo@gmail.com>
2023-12-10 12:20:45 -0300

    226. Rotas alinhadas (rotas filhas) parte 1

---
+++ src/components/vendas/Vendas.vue
@@ -0,0 +1,3 @@
+<template>
+    <h3>Vendas</h3>
+</template>
--- src/router.js
+++ src/router.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 //createWebHashHistory
+import Vendas from '@/components/vendas/Vendas.vue'
+import Servicos from '@/components/servicos/Servicos.vue'
 import Home from '@/views/Home.vue'
 import Login from '@/views/Login.vue'
 import Site from '@/views/Site.vue'
@@ -11,7 +13,11 @@ const routes = [
     },
     {
         path: '/home', //localhost:5173/home
-        component: Home
+        component: Home,
+        children: [
+            { path: 'vendas', component: Vendas },
+            { path: 'servicos', component: Servicos }
+        ]
     },
     {
         path: '/login', //localhost:5173/login
