3c5d52e52f4f853cfd25299027884d3edc74f136
Daniel Merlo <daniellmerlo@gmail.com>
2023-12-16 00:29:15 -0300

    234. Leads - Listando registros

--- src/components/vendas/Leads.vue
+++ src/components/vendas/Leads.vue
@@ -1,24 +1,45 @@
 <script setup>
-import { onMounted, ref } from "vue";
-
+import { onMounted, ref } from 'vue'
 
 const dados = ref([])
 
 function getDadosApi(url) {
   fetch(url)
-  .then(response => response.json())
-  .then(response => {
-    dados.value = response
-  })
+    .then((response) => response.json())
+    .then((response) => {
+      dados.value = response
+    })
 }
 
 onMounted(() => {
-  getDadosApi("http://0.0.0.0:3000/leads")
+  getDadosApi('http://0.0.0.0:3000/leads')
 })
-
 </script>
 
 <template>
-  <h3>Leads</h3>
-  {{ dados }}
+  <div>
+    <h3>Leads</h3>
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">NOME</th>
+          <th scope="col">TELEFONE</th>
+          <th scope="col"></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in dados" :key="item.id">
+          <td>{{ item.id }}</td>
+          <td>{{ item.nome }}</td>
+          <td>{{ item.telefone }}</td>
+          <td>
+            <router-link to="/home/vendas/leads" class="btn btn-sm btn-primary">
+              <i class="bi bi-pencil-square" style="cursor: pointer"></i>
+            </router-link>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
