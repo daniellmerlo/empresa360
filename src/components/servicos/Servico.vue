b93662a22997a5ee2cd9170b455a7bd8983e2742
Daniel Merlo <daniellmerlo@gmail.com>
2023-12-17 16:27:52 -0300

    247. Reagindo a alterações na url com beforeRouteUpdate

--- src/components/servicos/Servico.vue
+++ src/components/servicos/Servico.vue
@@ -1,18 +1,30 @@
 <script setup>
-import { onMounted, watch } from 'vue'
-import { useRoute } from 'vue-router'
+//import { onMounted, watch } from 'vue'
+import { onMounted } from 'vue'
+import { onBeforeRouteUpdate, useRoute } from 'vue-router'
 import ApiMixns from '@/mixins/ApiMixins'
 
 const { dados, getDadosApi } = ApiMixns()
 
 const route = useRoute()
 
+onBeforeRouteUpdate((to, from, next) => {
+  if (to.params.id != undefined) {
+    var url = `http://localhost:3000/servicos/${to.params.id}`
+    getDadosApi(url)
+  }
+
+  next()
+})
+
+/*
 watch(route, (to) => {
   if (to.params.id != undefined) {
     var url = `http://localhost:3000/servicos/${to.params.id}`
     getDadosApi(url)
   }
 })
+*/
 
 onMounted(() => {
   var url = `http://localhost:3000/servicos/${route.params.id}`
