21fda811e8e5c930e0f55c683785b2c64d48b44b
Daniel Merlo <daniellmerlo@gmail.com>
2023-12-17 12:51:28 -0300

    236. Pârametros de rotas parte 2 - Recebendo parâmetros via route.params

--- src/components/vendas/Lead.vue
+++ src/components/vendas/Lead.vue
@@ -1,3 +1,17 @@
+<script setup>
+//import { onMounted } from "vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+
+/*
+onMounted(() => {
+    console.log(route.params.id)
+})
+*/
+
+</script>
+
 <template>
-    <h3>Detalhe da Lead</h3>
+    <h3>Detalhe da Lead {{ route.params.id }}</h3>
 </template>
