b8a26380b44a2504d6dc02b1c68b7878702dfa8f
Daniel Merlo <daniellmerlo@gmail.com>
2023-12-17 22:35:50 -0300

    253. Contratos - Listando registros

--- src/components/vendas/Contratos.vue
+++ src/components/vendas/Contratos.vue
@@ -1,3 +1,40 @@
+<script setup>
+import { onMounted } from 'vue'
+
+import ApiMixns from '@/mixins/ApiMixins'
+
+const { dados, getDadosApi } = ApiMixns()
+
+onMounted(() => {
+  var url = 'http://localhost:3000/contratos'
+  getDadosApi(url)
+})
+</script>
+
 <template>
-  <h3>Contratos</h3>
+  <div>
+    <h3>Contratos</h3>
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">LEAD</th>
+          <th scope="col">SERVICO</th>
+          <th scope="col">DATA INICIO</th>
+          <th scope="col">DATA FIM</th>
+          <th scope="col"></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in dados" :key="item.id">
+          <td>{{ item.id }}</td>
+          <td>{{ item.leadId }}</td>
+          <td>{{ item.servicoId }}</td>
+          <td>{{ item.data_inicio }}</td>
+          <td>{{ item.data_fim }}</td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
